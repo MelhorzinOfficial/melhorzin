26b5676af99798cfc868da89bd4619d9449123f5
Vitor Lana <vitoresmerio00@gmail.com>
2025-03-15 18:30:49 -0300

    com bug

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <LocomotiveScrollProvider options={{ smooth: true, multiplier: 1, }}>
+      <App />
+    </LocomotiveScrollProvider>
+  </StrictMode>
+);
